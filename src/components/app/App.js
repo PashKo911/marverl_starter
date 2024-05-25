09e1fe9d721617283706bfa47fb23227441cb540
Pavel <rabota.pashok@gmail.com>
2024-05-25 18:43:35 +0300

    ls 176 completed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,12 @@
+import { lazy } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import { MainPage, ComicsPage } from '../pages'
+import { MainPage, ComicsPage, SingleComicPage } from '../pages'
 import AppHeader from '../appHeader/AppHeader'
+
+import Page404 from '../pages'
+
+const Page404 = lazy(() => import('../pages/404'))
 
 const App = () => {
 	return (
@@ -12,6 +17,8 @@ const App = () => {
 					<Routes>
 						<Route path="/" element={<MainPage />} />
 						<Route path="/comics" element={<ComicsPage />} />
+						<Route path="/comics/:comicId" element={<SingleComicPage />} />
+						<Route path="*" element={<Page404 />} />
 					</Routes>
 				</main>
 			</div>
