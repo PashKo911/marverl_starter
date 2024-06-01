ea9d2f5488926c02de5289d1c44ce5fc30e196ef
Pavel <rabota.pashok@gmail.com>
2024-06-01 12:09:00 +0300

    ls 190 completed

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import { useHttp } from '../hooks/http.hook'
 
 const useMarvelService = () => {
-	const { loading, request, error, clearError } = useHttp()
+	const { request, clearError, process, setProcess } = useHttp()
 
 	const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
 	const _apiKey = 'apikey=93f100f32a709b185b53e61657e81944'
@@ -59,9 +59,9 @@ const useMarvelService = () => {
 	}
 
 	return {
-		loading,
-		error,
 		clearError,
+		process,
+		setProcess,
 		getAllCharacters,
 		getCharacter,
 		getCharacterByName,
