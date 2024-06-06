9a9522af7edbb264d00897c3206cc664a0e53d71
Pavel <rabota.pashok@gmail.com>
2024-06-06 10:33:03 +0300

    react router paths changed

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -5,14 +5,17 @@ const AppHeader = () => {
 	return (
 		<header className="app__header">
 			<h1 className="app__title">
-				<Link to="/">
+				<Link to="/marverl_starter">
 					<span>Marvel</span> information portal
 				</Link>
 			</h1>
 			<nav className="app__menu">
 				<ul>
 					<li>
-						<NavLink end style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })} to="/">
+						<NavLink
+							end
+							style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })}
+							to="/marverl_starter">
 							Characters
 						</NavLink>
 					</li>
